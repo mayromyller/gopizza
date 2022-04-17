7bc71e760feebf5454289c3d329b7daa3f89fd2b
Mayro <deehmyller11@gmail.com>
2022-04-17 15:14:09 -0300

    chore: set up theme provider

--- App.tsx
+++ App.tsx
@@ -1,25 +1,28 @@
-import React from "react";
-import { View } from "react-native";
-import { StatusBar } from "expo-status-bar";
+import React from 'react'
+import { View } from 'react-native'
+import { StatusBar } from 'expo-status-bar'
 
-import AppLoading from "expo-app-loading";
+import AppLoading from 'expo-app-loading'
 
-import { useFonts, DMSans_400Regular } from "@expo-google-fonts/dm-sans";
-import { DMSerifDisplay_400Regular } from "@expo-google-fonts/dm-serif-display";
+import { useFonts, DMSans_400Regular } from '@expo-google-fonts/dm-sans'
+import { DMSerifDisplay_400Regular } from '@expo-google-fonts/dm-serif-display'
+
+import { ThemeProvider } from 'styled-components/native'
+import theme from '@src/global/theme'
 
 export default function App() {
   const [fontsLoaded] = useFonts({
     DMSans_400Regular,
-    DMSerifDisplay_400Regular,
-  });
+    DMSerifDisplay_400Regular
+  })
 
   if (!fontsLoaded) {
-    return <AppLoading />;
+    return <AppLoading />
   }
 
   return (
-    <View>
+    <ThemeProvider theme={theme}>
       <StatusBar style="auto" />
-    </View>
-  );
+    </ThemeProvider>
+  )
 }
