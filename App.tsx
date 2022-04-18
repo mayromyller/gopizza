af88332f31ff1f6a449c79a91b172e4280444129
Mayro <deehmyller11@gmail.com>
2022-04-18 18:03:31 -0300

    chore: add SiginScreen and config StatusBar

--- App.tsx
+++ App.tsx
@@ -1,14 +1,13 @@
 import React from 'react'
-import { View } from 'react-native'
 import { StatusBar } from 'expo-status-bar'
-
 import AppLoading from 'expo-app-loading'
 
 import { useFonts, DMSans_400Regular } from '@expo-google-fonts/dm-sans'
 import { DMSerifDisplay_400Regular } from '@expo-google-fonts/dm-serif-display'
 
 import { ThemeProvider } from 'styled-components/native'
-import theme from '@src/global/theme'
+import { SignIn } from '@screens/SignIn'
+import theme from './src/global/theme'
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -22,7 +21,8 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
-      <StatusBar style="auto" />
+      <StatusBar style="light" translucent backgroundColor="transparent" />
+      <SignIn />
     </ThemeProvider>
   )
 }
