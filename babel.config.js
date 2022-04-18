07f1282e63fbbeb5e3a8fffdae123a9d70520d9f
Mayro <deehmyller11@gmail.com>
2022-04-17 21:49:15 -0300

    fix: adjusts babel config

--- babel.config.js
+++ babel.config.js
@@ -1,23 +1,21 @@
-module.exports = function(api) {
-  api.cache(true);
+module.exports = function (api) {
+  api.cache(true)
   return {
     presets: ['babel-preset-expo'],
     plugins: [
-      'react-native-reanimated/plugin'
+      'react-native-reanimated/plugin',
       [
         'module-resolver',
         {
-          root: './src',
-          extensions: [
-            '.ts', '.tsx', '.js', '.json'
-          ], 
+          root: ['./src'],
+          extensions: ['.ts', '.tsx', '.jsx', '.js', '.json'],
           alias: {
             '@components': './src/components',
             '@screens': './src/screens',
-            '@assets': './src/assets',
+            '@assets': './src/assets'
           }
         }
       ]
-    ],
-  };
-};
+    ]
+  }
+}
