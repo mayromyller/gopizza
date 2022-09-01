3c9fff775262a87be2ecb2d062648a6a4921f930
Mayro <deehmyller11@gmail.com>
2022-09-01 14:05:03 -0300

    chore: add tabs routes and verify with user is admin to redirect specific route

--- src/routes/user.stack.routes.tsx
+++ src/routes/user.stack.routes.tsx
@@ -2,19 +2,33 @@ import React from 'react'
 
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 
-import { Home, Product } from '@screens/exports'
+import { Home, Product, Order } from '@screens/exports'
 
-const { Navigator, Screen } = createNativeStackNavigator()
+import { useAuth } from '../hooks/auth'
+import { UserTabRoutes } from './user.tabs.routes'
+
+const { Navigator, Screen, Group } = createNativeStackNavigator()
 
 export function UserStackRoutes() {
+  const { user } = useAuth()
+
   return (
     <Navigator
       screenOptions={{
         headerShown: false
       }}
     >
-      <Screen name="home" component={Home} />
-      <Screen name="product" component={Product} />
+      {user?.isAdmin ? (
+        <Group>
+          <Screen name="home" component={Home} />
+          <Screen name="product" component={Product} />
+        </Group>
+      ) : (
+        <Group>
+          <Screen name="UserTabRoutes" component={UserTabRoutes} />
+          <Screen name="order" component={Order} />
+        </Group>
+      )}
     </Navigator>
   )
 }
