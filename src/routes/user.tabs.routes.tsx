2204b1956b39cf3d3d371d6295f39f5a63f15daa
Mayro <deehmyller11@gmail.com>
2022-09-01 14:04:00 -0300

    chore: add tabs routes

---
+++ src/routes/user.tabs.routes.tsx
@@ -0,0 +1,50 @@
+import React from 'react'
+
+import { Platform } from 'react-native'
+
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import { useTheme } from 'styled-components/native'
+
+import { Orders, Home } from '@screens/exports'
+
+import { BottomMenu } from '@components/BottomMenu'
+
+const { Navigator, Screen } = createBottomTabNavigator()
+
+export function UserTabRoutes() {
+  const { COLORS } = useTheme()
+
+  return (
+    <Navigator
+      screenOptions={{
+        tabBarActiveTintColor: COLORS.SECONDARY_900,
+        tabBarInactiveTintColor: COLORS.SECONDARY_400,
+        headerShown: false,
+        tabBarShowLabel: false,
+        tabBarStyle: {
+          height: 80,
+          paddingVertical: Platform.OS === 'ios' ? 20 : 0
+        }
+      }}
+    >
+      <Screen
+        name="home"
+        component={Home}
+        options={{
+          tabBarIcon: ({ color }) => (
+            <BottomMenu title="Cardápio" color={color} />
+          )
+        }}
+      />
+      <Screen
+        name="orders"
+        component={Orders}
+        options={{
+          tabBarIcon: ({ color }) => (
+            <BottomMenu title="Pedido" color={color} notifications="0" />
+          )
+        }}
+      />
+    </Navigator>
+  )
+}
