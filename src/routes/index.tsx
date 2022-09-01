cc4ac73bc59f2da732057a937b3fd2b51c491a3b
Mayro <deehmyller11@gmail.com>
2022-09-01 14:06:46 -0300

    chore: update routes with SignIn screen and useAuth hook for protected routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,11 +3,15 @@ import React from 'react'
 import { NavigationContainer } from '@react-navigation/native'
 
 import { UserStackRoutes } from './user.stack.routes'
+import { SignIn } from '@screens/SignIn'
+
+import { useAuth } from '../hooks/auth'
 
 export function Routes() {
+  const { user } = useAuth()
   return (
     <NavigationContainer>
-      <UserStackRoutes />
+      {user ? <UserStackRoutes /> : <SignIn />}
     </NavigationContainer>
   )
 }
