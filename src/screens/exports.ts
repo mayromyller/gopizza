6f0453701948d417b658cbe62ae48f068580eefe
Mayro <deehmyller11@gmail.com>
2022-09-01 14:07:30 -0300

    chore: update exports screens

--- src/screens/exports.ts
+++ src/screens/exports.ts
@@ -1,5 +1,7 @@
 import { SignIn } from '@screens/SignIn'
 import { Product } from '@screens/Product'
 import { Home } from '@screens/Home'
+import { Order } from '@screens/Order'
+import { Orders } from '@screens/Orders'
 
-export { SignIn, Product, Home }
+export { SignIn, Product, Home, Order, Orders }
